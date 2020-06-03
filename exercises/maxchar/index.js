627e3d68d117da3a1ed02eb0e8f2dc02aeefb6b5
jessesbyers <jesse.s.byers@gmail.com>
2020-06-03 16:04:45 -0400

    added maxChars

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(string) {
+    // placeholder object to hold counts of characters
+    let counts = {}
+
+    // loop through each character in the string, check if there is a key for that character, 
+    // add 1 if yes, set equal to one if no
+    for (i=0; i<string.length; i++) {
+        counts[string[i]] ? counts[string[i]] = counts[string[i]] + 1 : counts[string[i]] = 1
+    }
+
+    // get max value by making array of all values, sorting descending, and pulling index 0
+    let maxValue = Object.values(counts).sort((a, b) => a<b ? 1 : -1)[0]
+
+    // get key for that value by making array of keys, then find the key that has a value of maxValue
+    var maxKey = Object.keys(counts).find(count => counts[count] === maxValue)
+
+    return maxKey
+}
+
 
 module.exports = maxChar;
