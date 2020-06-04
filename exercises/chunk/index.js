a9ce55d8eaca7c9601beb3cc05b9bc5c95c7e64e
jessesbyers <jesse.s.byers@gmail.com>
2020-06-03 20:06:15 -0400

    added chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,29 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// function chunk(array, size) {
+//     let chunkedArray = []
+  
+//     for (let i=0; i<array.length; i++) {
+//       if (chunkedArray[chunkedArray.length-1] && chunkedArray[chunkedArray.length-1].length < size) {
+//         chunkedArray[chunkedArray.length-1].push(array[i])
+//       } else {
+//         chunkedArray.push([array[i]])
+//       }
+//     }
+//     return chunkedArray
+//   }
+
+function chunk(array, size) {
+    let chunkedArray = []
+    let i=0
+  
+    while (i<array.length) {
+      let piece = array.slice(i, i+size)
+      chunkedArray.push(piece)
+        i=i+size
+    }
+    return chunkedArray
+  }
 
 module.exports = chunk;
