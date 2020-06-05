7bcde26902eab5f4b6e12a6362e6cad522fcd9cf
jessesbyers <jesse.s.byers@gmail.com>
2020-06-05 11:26:16 -0400

    added steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,56 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// MY SOLUTION - with repeat method
+// function steps(n) {
+//     for (i=1; i<=n; i++) {
+//         string = "#".repeat(i) + " ".repeat(n-i)
+//         console.log(string)
+//     }
+// }
+
+// SOLUTION 1 - using matrix of rows and columns - best interview solution
+// function steps(n) {
+//     // iterate through rows
+//     for (let i=0; i<n; i++) {
+//         let stair = ""
+//         // iterate through columns
+//         for (let ind=0; ind<n; ind++) {
+//             if (ind <= i) {
+//                 stair += "#"
+//             } else {
+//                 stair += " "
+//             }
+//         }
+//         console.log(stair)
+//     }
+// }
+
+
+
+
+// SOLUTION 2 - WITH RECURSION - more complex solution
+function steps(n, row = 0, step = '') {
+    if (n === row) {
+        return; 
+    } 
+
+    if (n === step.length) {
+        console.log(step)
+        return steps(n, row+1)   
+    }
+
+    if (step.length <= row) {
+        step += "#"
+    } else {
+        step += " "
+    }
+
+    steps(n, row, step)
+}
+
+
+
+
 
 module.exports = steps;
