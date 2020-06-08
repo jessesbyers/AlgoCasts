501968268f1cb75d7cea0b8e5131d39d68164147
jessesbyers <jesse.s.byers@gmail.com>
2020-06-08 11:18:33 -0400

    added matrix spiral

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,54 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+// function matrix(n) {}
+
+
+function matrix(n) {
+    let results = []
+    for (let row = 0; row < n; row++) {
+      let rowArray = []
+      results.push(rowArray)
+    }
+  
+    let counter = 1
+  
+    let startRow = 0
+    let endRow = n-1
+    let startCol = 0
+    let endCol = n-1
+  
+    while (startCol <= endCol && startRow <= endRow) {
+      // start row
+      for (let i = startCol; i <= endCol; i++) {
+        results[startRow][i] = counter
+        counter++
+      }
+      startRow++
+  
+      // end column
+      for (let i = startRow; i <= endRow; i++) {
+        results[i][endCol] = counter
+        counter++
+      }
+      endCol--
+  
+      // end row
+      for (let i = endCol; i >= startCol; i--) {
+        results[endRow][i] = counter
+        counter++
+      }
+      endRow--
+  
+      // start row
+      for (let i = endRow; i >= startRow; i--) {
+        results[i][startCol] = counter
+        counter++
+      }
+      startCol++
+    }
+  
+    return results
+  }
 
 module.exports = matrix;
